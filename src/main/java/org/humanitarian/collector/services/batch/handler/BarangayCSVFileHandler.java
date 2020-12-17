7de86051cbde084d07fc96d66dcd0b628b329864
Jan Tan <jmgtan@gmail.com>
2020-12-17 13:43:04 +0800

    Added support for batch upload for Barangay form data

--- src/main/java/org/humanitarian/collector/services/batch/handler/BarangayCSVFileHandler.java
+++ src/main/java/org/humanitarian/collector/services/batch/handler/BarangayCSVFileHandler.java
@@ -2,16 +2,16 @@ package org.humanitarian.collector.services.batch.handler;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.apache.commons.io.input.BOMInputStream;
-import org.humanitarian.collector.controllers.requests.DemographicFormDataRequest;
+import org.humanitarian.collector.controllers.requests.BarangayFormDataBatchRow;
+import org.humanitarian.collector.controllers.requests.BarangayFormDataRequest;
 import org.humanitarian.collector.exceptions.InvalidFileFormatException;
+import org.humanitarian.collector.models.BarangayFormData;
 import org.humanitarian.collector.models.BatchDataFile;
-import org.humanitarian.collector.models.DemographicFormData;
 import org.humanitarian.collector.services.SubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -19,26 +19,25 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Service
-public class DemographicCSVFileHandler implements FileHandler<DemographicFormData> {
+@Service
+public class BarangayCSVFileHandler implements FileHandler<BarangayFormData> {
 
     @Autowired
     private SubmissionService submissionService;
 
     @Override
     @Transactional
-    public List<DemographicFormData> handle(BatchDataFile batchDataFile, InputStream inputStream) throws InvalidFileFormatException, IOException {
+    public List<BarangayFormData> handle(BatchDataFile batchDataFile, InputStream inputStream) throws InvalidFileFormatException, IOException {
         BOMInputStream bin = new BOMInputStream(inputStream);
 
-        List<DemographicFormDataRequest> rows = new CsvToBeanBuilder<DemographicFormDataRequest>(new InputStreamReader(bin, StandardCharsets.UTF_8))
-                .withType(DemographicFormDataRequest.class)
-                .build()
-                .parse();
+        List<BarangayFormDataBatchRow> rows = new CsvToBeanBuilder<BarangayFormDataBatchRow>(new InputStreamReader(bin, StandardCharsets.UTF_8))
+                .withType(BarangayFormDataBatchRow.class)
+                .build().parse();
 
-        List<DemographicFormData> results = new ArrayList<>(rows.size() + 1);
+        List<BarangayFormData> results = new ArrayList<>(rows.size() + 1);
 
-        for (DemographicFormDataRequest kdr : rows) {
-            results.add(submissionService.saveDemographicFormDataWithBatch(kdr, batchDataFile));
+        for (BarangayFormDataBatchRow row : rows) {
+            results.addAll(submissionService.saveBarangayFormDataRequest(row.toFormDataRequest()));
         }
 
         return results;
@@ -46,7 +45,7 @@ public class DemographicCSVFileHandler implements FileHandler<DemographicFormDat
 
     @Override
     public String getIdentifier() {
-        return "demographic";
+        return "barangay";
     }
 
     @Override
